f07506dbb6441fff41c1ec1f7aa55182135e8d3e
Fiona <frf@andrew.cmu.edu>
2023-10-31 18:36:56 -0400

    Adding more bytes to system memory.

--- src/stream_cipher.sv
+++ src/stream_cipher.sv
@@ -8,25 +8,30 @@ module tt_um_Fiona_CMU (
   input  logic       clk,
   input  logic       rst_n);
 
-  logic [7:0]       out, indrive, ct, in, index, lsr_low, lsr_high;
-  logic             view, inc, enable, clock, reset_n;
-  logic [31:0] lsr; //max 16 characters with a buffer byte for calculation
+  logic [7:0]       out, ct, index, lsr_low, lsr_high;
+  logic             view, inc;
+  logic [63:0] lsr;
+  //max 8 characters (bytes) can be held at a time
+  //pushing beyond this limit will lose previous characters
+  //all values except for the last can still be encrypted in this case
 
   assign uio_out = 8'b0000_0000;
   assign uio_oe  = 8'b0000_0000;
   assign uo_out  = out;
 
-  assign ct = ui_in[1:0];
+  assign ct = ui_in[2:0];
+  //when not encrypting, used to control which byte of data is being viewed
 
-  assign view    = uio_in[2];
+  assign view = uio_in[2];
   //if high, view encrypted message
   //if low,  view decrypted message
 
   assign encrypt = uio_in[1];
-  //encrypt if high
+  //if high, message is stored and encrypted
+  //if low,  message can only be viewed
 
   assign inc   = uio_in[0];
-  //increment the state (go to next symbol)
+  //used when encryting to indicate to the system that input is valid
 
   always_ff@ (posedge inc, negedge rst_n) begin
     if(~rst_n) lsr <= 0;
@@ -37,18 +42,27 @@ module tt_um_Fiona_CMU (
     if(~rst_n | encrypt) out <= 0;
     else begin
      if (view) begin
+       //viewing encrypted message
        if      (ct == 0) out <= lsr[7:0];
        else if (ct == 1) out <= lsr[15:8];
        else if (ct == 2) out <= lsr[23:16];
-       else              out <= lsr[31:24];
+       else if (ct == 3) out <= lsr[31:24];
+       else if (ct == 4) out <= lsr[39:32];
+       else if (ct == 5) out <= lsr[47:40];
+       else if (ct == 6) out <= lsr[55:48];
+       else              out <= lsr[63:56];
       end
      else begin
+       //viewing decrypted message
        if      (ct == 0) out <= lsr[7:0]   ^ lsr[15:8];
        else if (ct == 1) out <= lsr[15:8]  ^ lsr[23:16];
        else if (ct == 2) out <= lsr[23:16] ^ lsr[31:24];
-       else              out <= lsr[31:24];
+       else if (ct == 3) out <= lsr[31:24] ^ lsr[39:32];
+       else if (ct == 4) out <= lsr[39:32] ^ lsr[47:40];
+       else if (ct == 5) out <= lsr[47:40] ^ lsr[55:48];
+       else if (ct == 6) out <= lsr[55:48] ^ lsr[63:56];
+       else              out <= lsr[63:56];
      end
     end
   end
-
 endmodule: tt_um_Fiona_CMU
